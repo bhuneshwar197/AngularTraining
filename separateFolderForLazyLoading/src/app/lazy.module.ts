09ebd4aefa85ca2f971198b9ba44134b0983b1cb
Bhuneshwar Kumar <bhuneshwar197@gmail.com>
2019-07-17 15:00:12 +0530

    added lazyLoadingComponent

---
+++ separateFolderForLazyLoading/src/app/lazy.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ParentLazyComponent } from './parent-lazy/parent-lazy.component';
+import { ChildLazyComponent } from './child-lazy/child-lazy.component';
+
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes= [
+  {
+    path: 'load-me', component: ParentLazyComponent
+  }
+]
+
+
+
+@NgModule({
+  declarations: [ParentLazyComponent, ChildLazyComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes)
+  ]
+})
+export class LazyModule { }
